26b4866a2463cc2353f8939ff8caf6e6c08f7b68
kyumin <rhkdtlrtm12@hanyang.ac.kr>
2021-10-21 21:13:44 +0900

    feat: modal 창 구현

--- velog-clone/main.js
+++ velog-clone/main.js
@@ -4,7 +4,6 @@
 
 const navDates = document.querySelector(".nav__dates");
 const navDate = document.querySelector(".nav__date");
-const navDateText = document.querySelector(".nav__dates-text");
 
 navDates.addEventListener("click", () => {
     if (navDate.style.visibility == "hidden") {
@@ -15,6 +14,8 @@ navDates.addEventListener("click", () => {
 });
 
 // 1-b. navDate 클릭 시 , 안에 텍스트가 바뀐다
+
+const navDateText = document.querySelector(".nav__dates-text");
 navDate.addEventListener("click", (e) => {
     navDateText.innerText = e.target.innerText;
     
@@ -26,4 +27,40 @@ navDate.addEventListener("click", (e) => {
     e.target.classList.add("active");
 });
 
-// 2. modal 띄우기
+// 2. modal 띄우기(팟장님 코드 참고)
+const postContainer = document.querySelector(".main-screen");
+// 화면 클릭을 받아오자.
+
+postContainer.addEventListener("click", (e) => {
+    let el = e.target;
+    while(el.classList && !el.classList.contains("post")) {
+        el = el.parentNode;
+    }
+    // e.target이 post일 때까지 탐색
+    if (!el.classList || el.classList.contains("post__modal")) return;
+    // el이 post 외부가 눌러졌거나 띄어진 modal의 post를 클릭했을 때는 반응하지 않도록
+
+    const modalBg = document.createElement("div");
+    const modalPost = document.createElement("section");
+    const modalCloseBtn = document.createElement("button");
+
+    modalBg.setAttribute("class", "post__modal-bg");
+
+    modalPost.innerHTML = el.innerHTML;
+    modalPost.setAttribute("class", "post post__modal");
+
+    modalCloseBtn.innerText = "X";
+    modalCloseBtn.setAttribute("class", "post__modal-close-btn");
+
+    modalBg.appendChild(modalPost);
+    postContainer.appendChild(modalBg);
+    modalPost.appendChild(modalCloseBtn);
+
+    document.body.style.overflow = "hidden";
+    // modal이 띄어졌을 때는 화면이 스크롤 되지 않도록 한다.
+    modalCloseBtn.addEventListener("click", () => {
+        modalBg.remove();
+        document.body.style.overflow = "visible";
+        // modal이 사라지고, 스크롤 되도록 만들기
+    });
+});
