c6c90551c7ee106ff0efeb874addecc3307aa5b0
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2021-12-01 09:36:35 +0900

    header 스타일링

--- velog-clone-react/src/components/common/Header.jsx
+++ velog-clone-react/src/components/common/Header.jsx
@@ -1,24 +1,66 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Header = () => {
-    return (
-        <div>
+  return (
+    <StyledWrapper>
+      <div>
         <StyledLeft>
-            sopt.log
+          <div>v</div>
+          <div>q-min-sopt.log</div>
         </StyledLeft>
         <StyledRight>
-            <Link to="/write" >
-                <button>새 글 작성</button>
-            </Link>
+          <StyledLink to="/write">
+            <button>새 글 작성</button>
+          </StyledLink>
         </StyledRight>
-        </div>
-    )
-}
+      </div>
+    </StyledWrapper>
+  );
+};
 
 export default Header;
 
-const StyledLeft = styled.div``;
+const StyledWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
 
+  & > div {
+    width: 64rem;
+    height: 4rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+const StyledLeft = styled.div`
+  display: flex;
+  font-size: 1.5rem;
+  align-items: center;
+
+  & > div:first-child {
+    width: 1.75rem;
+    height: 1.75rem;
+    text-align: center;
+    background-color: black;
+    color: white;
+    border-radius: 10%;
+    margin-right: 1rem;
+  }
+`;
 const StyledRight = styled.div``;
+const StyledLink = styled(Link)`
+  & > button {
+    background-color: white;
+    font-size: 1rem;
+    width: 6rem;
+    height: 2rem;
+    border-style: none;
+    border: 0.1rem solid black;
+    border-radius: 1rem;
+  }
+`;
