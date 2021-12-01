b8d6315d4f5a222a3786bc5089a76d0fde8b7dff
kimkyumin <rhkdtlrtm12@hanyang.ac.kr>
2021-12-01 15:55:30 +0900

    ArticleCard 스타일링 구현

--- velog-clone-react/src/components/home/ArticleCard.jsx
+++ velog-clone-react/src/components/home/ArticleCard.jsx
@@ -1,20 +1,55 @@
 import React from "react";
+import styled from "styled-components";
 
 const ArticleCard = ({ article }) => {
   console.log("article", article);
 
   const { title, summary, tags, thumbnail, date } = article;
   return (
-    <div>
-      <img src={thumbnail} alt="" />
-      <h3>{title}</h3>
-      <h4>{summary}</h4>
-      {tags.map((tag) => (
-        <span key={tag}>{tag}</span>
-      ))}
-      <div>{date}</div>
-    </div>
+    <StyledWrapper>
+      <div>
+        <img src={thumbnail} alt="" />
+        <h3>{title}</h3>
+        <h4>{summary}</h4>
+        <Tags>
+          {tags.map((tag) => (
+            <div key={tag}>{tag}</div>
+          ))}
+        </Tags>
+        <div>{date}</div>
+      </div>
+    </StyledWrapper>
   );
 };
 
 export default ArticleCard;
+
+const StyledWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10rem;
+
+  & > div {
+    width: 48rem;
+  }
+
+  & > div > img {
+    width: 100%;
+    height: auto;
+  }
+`;
+
+const Tags = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+
+  & > div {
+    padding: 0.4rem 0.7rem;
+    border-radius: 0.7rem;
+    background-color: #f8f9fa;
+    color: green;
+    margin-right: 1rem;
+  }
+`;
